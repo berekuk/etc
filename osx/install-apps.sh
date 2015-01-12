6807619602636958eb92cb469fc90ef490531317
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2015-01-13 02:11:27 +0300

    install macvim

--- osx/install-apps.sh
+++ osx/install-apps.sh
@@ -2,6 +2,6 @@
 
 brew install caskroom/cask/brew-cask
 
-for cask in adium alfred anki balsamiq-mockups dash dropbox evernote firefox flux iterm2 lighttable sketch skype transmission vlc ynab; do
+for cask in adium alfred anki balsamiq-mockups dash dropbox evernote firefox flux iterm2 lighttable macvim sketch skype transmission vlc ynab; do
     brew cask install $cask
 done
