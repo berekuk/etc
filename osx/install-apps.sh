033c2e5d643645e933b7a09242b4e1a947bdd4a4
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2015-05-19 16:17:54 +0300

    update osx apps list

--- osx/install-apps.sh
+++ osx/install-apps.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
 brew install caskroom/cask/brew-cask
 
-for cask in adium alfred anki balsamiq-mockups dash dropbox evernote firefox flux iterm2 lighttable macvim sketch skype transmission vlc ynab; do
+for cask in adium alfred anki balsamiq-mockups dash dropbox evernote firefox flux iterm2 sketch skype transmission vlc ynab; do
     brew cask install $cask
 done
