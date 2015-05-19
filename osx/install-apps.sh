48b2cf26fbc8ffa089b55dba7a4d29dfc10dab60
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2015-05-19 17:46:48 +0300

    brew install command-line packages

--- osx/install-apps.sh
+++ osx/install-apps.sh
@@ -3,6 +3,15 @@ set -e
 
 brew install caskroom/cask/brew-cask
 
-for cask in adium alfred anki balsamiq-mockups dash dropbox evernote firefox flux iterm2 sketch skype transmission vlc ynab; do
+for cask in 1password adium alfred anki balsamiq-mockups dash dropbox evernote firefox flux iterm2 sketch skype transmission vlc ynab karabiner seil slate; do
     brew cask install $cask
 done
+
+brew install \
+    emacs vim \
+    git mercurial subversion \
+    docker docker-compose docker-machine \
+    gcc ghc cabal-install go rust \
+    node pypy python python3 \
+    rbenv \
+    ansible bash-completion jq sqlite tmux wget
