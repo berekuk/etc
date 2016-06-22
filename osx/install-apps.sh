11ea7e35178916f53f761d544f990a3a5d991b86
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2016-06-22 14:20:15 +0300

    brain-workshop and emacs-plus in osx brew script

--- osx/install-apps.sh
+++ osx/install-apps.sh
@@ -3,7 +3,7 @@ set -e
 
 brew install caskroom/cask/brew-cask
 
-for cask in 1password adium alfred anki balsamiq-mockups dash dropbox evernote firefox flux iterm2 sketch skype transmission vlc ynab karabiner seil slate; do
+for cask in 1password adium alfred anki balsamiq-mockups brain-workshop dash dropbox evernote firefox flux iterm2 sketch skype transmission vlc ynab karabiner seil slate; do
     brew cask install $cask
 done
 
@@ -15,3 +15,7 @@ brew install \
     node pypy python python3 \
     rbenv \
     ansible bash-completion jq sqlite tmux wget
+
+brew tap d12frosted/emacs-plus
+brew install emacs-plus --with-cocoa --with-gnutls --with-librsvg --with-imagemagick --with-spacemacs-icon
+mv $(ls -1 /usr/local/Cellar/emacs/ | tail -1)/Emacs.app /Applications/
